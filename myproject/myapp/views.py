38628832d0377a45e0be0087b362265aaba6791e
jss123987 <justinsmith100297@gmail.com>
2020-02-10 21:40:04 -0500

    worked with templates

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def hello(request):
- return render(request, 'myapp/hello.html', {})
+ posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+ return render(request, 'myapp/hello.html', {'posts':posts})
