ec473523f7f6e354aeea916b44c0232f0da0fcd6
jss123987 <justinsmith100297@gmail.com>
2020-02-13 20:45:06 -0500

    ugh

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -119,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
