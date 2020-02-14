34825ec361bd1841bf626421b17572157b3fc978
jss123987 <justinsmith100297@gmail.com>
2020-02-14 09:31:51 -0500

    1post problem

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from django.utils import timezone
 
 def hello(request):
- posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+ posts = Post.objects.order_by('published_date')
  return render(request, 'myapp/hello.html', {'posts':posts})
 
 def post_detail(request, pk):
