d8c42e135092f4c3c1c95a1a23040737775b0992
jss123987 <justinsmith100297@gmail.com>
2020-02-16 20:12:30 -0500

    idek the attempt #

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -13,15 +13,6 @@ def post_detail(request, pk):
  return render(request, 'myapp/post_detail.html', {'post':post})
 
 def post_new(request):
- if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-        return redirect( post_detail, pk=post.pk)
- else:
-  form = PostForm()
+ form = PostForm()
  return render(request, 'myapp/post_edit.html', {'form': form})
 
