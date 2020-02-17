6df050385f57d35ccffa0435bd1346a7fce6671e
jss123987 <justinsmith100297@gmail.com>
2020-02-17 14:02:08 -0500

    meh

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -4,4 +4,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
  class Meta:
   model = Post
+  widgets = {
+            'text': forms.Textarea(attrs={'rows':4, 'cols':15}),
+  }
   fields = ('title', 'text', 'category')
